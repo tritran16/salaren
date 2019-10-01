4f350602f49fd881cbc6d8095ff8fc61b8240d12
root <root@mx17114.superdata.vn>
2019-10-01 13:43:27 +0000

    [init] salaren

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mariadb';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'salaren';
+$CFG->dbuser    = 'root';
+$CFG->dbpass    = '1q2w3e4r';
+$CFG->prefix    = 'slr_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+$CFG->wwwroot   = 'http://45.117.171.14/salaren';
+$CFG->dataroot  = '/var/_salarendata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
